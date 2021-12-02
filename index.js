8bfff268640cd039d281c577e768a34cbb7960f9
StrategicKSA <whitewalker12@gmail.com>
2021-12-02 07:52:22 -0800

    commit to master

--- index.js
+++ index.js
@@ -2,31 +2,31 @@
 import express from 'express';
 //importing the mongoose package
 import mongoose from 'mongoose';
-dotenv.config();
+
 //importing the dotenv package
 import dotenv from 'dotenv';
-
+dotenv.config();
 //creating an instance of the package express
 const app = express();
 
 //creating a port for the server
-const PORT = 5000;
+const PORT = process.env.PORT ?? 5000;
 //creating a routesss
-app.get('/', (req, res)=>{
-res.send("Easy On Me")
+app.get('/', (req, res) => {
+    res.send("Easy On Me")
 });
 
 
-mongoose.connect(process.env.MONGO_DB_CONSTRING, (error)=>{
+mongoose.connect(process.env.MONGO_DB_CONSTRING, (error) => {
     //creating a server for the port
 
-if(error){
-    return console.log("connection unable to connect to MongoDB");
-}
-else{
-    console.log("connection to MongoDB successful");
+    if (error) {
+        return console.log("connection unable to connect to MongoDB");
+    }
+    else {
+        console.log("connection to MongoDB successful");
 
-    app.listen(PORT, ()=>
-console.log(`server up and listening to request on ${PORT}`));
-}
+        app.listen(PORT, () =>
+            console.log(`server up and listening to request on ${PORT}`));
+    }
 });
